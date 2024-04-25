9c52a231beec5cc6144ee058ce3137e3a61c6615
Paulo Silva <pauloalmeida1288@gmail.com>
2024-04-26 00:02:29 +0100

    Classe Bat e bee adicionado

---
+++ AnimalKingdom/Bat.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimalKingdom
+{
+    public class Bat : Animal
+    {
+        public override string Sound()
+        {
+            return base.Sound() + "<Click>";
+        }
+    }
+}
